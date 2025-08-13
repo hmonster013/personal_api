64c5dee83c085737ca282ee641b10f1e9e250e6e
hmonster013 <tranphuhuyit@gmail.com>
2025-08-13 16:44:00 +0700

    [ADD] Search blogs by kw, skills, skill_ids

--- info/filters.py
+++ info/filters.py
@@ -4,15 +4,37 @@ from info.models import Blogs, Experiences, Projects
 
 class BlogsFilter(filters.FilterSet):
     kw = filters.CharFilter(method='filter_kw')
+    skills = filters.CharFilter(method='filter_skills')
+    skill_ids = filters.CharFilter(method='filter_skill_ids')
 
     class Meta:
         model = Blogs
-        fields = ['kw']
+        fields = ['kw', 'skills', 'skill_ids']
 
     def filter_kw(self, queryset, name, value):
+        """Tìm kiếm theo keyword trong title, content, description"""
         return queryset.filter(title__icontains=value) | \
             queryset.filter(content__icontains=value) | \
             queryset.filter(description__icontains=value)
+
+    def filter_skills(self, queryset, name, value):
+        """Tìm kiếm theo tên skill (có thể truyền nhiều skill cách nhau bởi dấu phẩy)"""
+        if not value:
+            return queryset
+
+        skill_names = [skill.strip() for skill in value.split(',')]
+        return queryset.filter(skills__name__in=skill_names).distinct()
+
+    def filter_skill_ids(self, queryset, name, value):
+        """Tìm kiếm theo ID của skill (có thể truyền nhiều ID cách nhau bởi dấu phẩy)"""
+        if not value:
+            return queryset
+
+        try:
+            skill_ids = [int(skill_id.strip()) for skill_id in value.split(',')]
+            return queryset.filter(skills__id__in=skill_ids).distinct()
+        except ValueError:
+            return queryset.none()
 
 class ExperiencesFilter(filters.FilterSet):
     kw = filters.CharFilter(method='filter_kw')
--- info/urls.py
+++ info/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
 
-from info.views import BlogsDetailView, BlogsListView, ExperiencesDetailView, ExperiencesListView, ProjectsDetailView, ProjectsListView
+from info.views import (
+    BlogsDetailView, BlogsListView, BlogsSearchView,
+    ExperiencesDetailView, ExperiencesListView,
+    ProjectsDetailView, ProjectsListView
+)
 
 app_name = 'info'
 
 urlpatterns = [
     path('blogs/list', BlogsListView.as_view(), name='blogs-list'),
+    path('blogs/search', BlogsSearchView.as_view(), name='blogs-search'),
     path('blogs/view/<int:id>', BlogsDetailView.as_view(), name='blogs-detail'),
     path('experiences/list', ExperiencesListView.as_view(), name='experiences-list'),
     path('experiences/view/<int:id>', ExperiencesDetailView.as_view(), name='experiences-detail'),
